fdde511797cd82f34c0584760b3f1ceaf502db47
Adeolu Babatunde <adeolutechs@gmail.com>
2018-03-15 19:57:54 -0500

    Fixed issues

--- script/navbar.js
+++ script/navbar.js
@@ -48,35 +48,6 @@ addLoadEvent(function() {
         state.xDown = evt.touches[0].clientX;
         state.yDown = evt.touches[0].clientY;
     };
-
-    function handleTouchMove(evt) {
-        const xDown = state.xDown;
-        const yDown = state.yDown;
-
-        if (!xDown || !yDown) {
-            return;
-        }
-
-        var xUp = evt.touches[0].clientX;
-        var yUp = evt.touches[0].clientY;
-
-        var xDiff = xDown - xUp;
-        var yDiff = yDown - yUp;
-
-        if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
-            if (xDiff > 0) {
-                open()
-            } else {
-                open()
-            }
-        } else {
-            if (yDiff > 0) {
-                /* up swipe */
-            } else {
-                /* down swipe */
-            }
-        }
-    }
 });
 
 // function open() {
