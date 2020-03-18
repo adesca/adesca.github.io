c4ef86958a159a7c64bda603b8718876723f650f
Adeolu Babatunde <adeolutechs@gmail.com>
2020-03-18 12:50:04 -0500

    added pandemic game

---
+++ pandemic-game/precache-manifest.faa002947b633153e17c99b72b5d5b81.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318af5abdb67ea51b7d435241962d4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51b43ebd1730edee0366",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "03e80b049a6f6c084e0b",
+    "url": "/static/css/main.01264426.chunk.css"
+  },
+  {
+    "revision": "51b43ebd1730edee0366",
+    "url": "/static/js/2.3bb27c42.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3bb27c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e80b049a6f6c084e0b",
+    "url": "/static/js/main.82cfe285.chunk.js"
+  },
+  {
+    "revision": "860d3b1c228292e6f5e6",
+    "url": "/static/js/runtime-main.ac2d2099.js"
+  },
+  {
+    "revision": "bf8b6ea52ab48231507d404975662528",
+    "url": "/static/media/legend.bf8b6ea5.jpeg"
+  },
+  {
+    "revision": "4b91ef39b089f868774a1758e4df5781",
+    "url": "/static/media/mainBoard.4b91ef39.png"
+  }
+]);
